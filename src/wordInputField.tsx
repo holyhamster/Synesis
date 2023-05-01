5c821c8967f0a635bcb7a7fa148d55fdf7aaa80a
Dmitrii Mv <hotrhamster@gmail.com>
2023-05-01 10:25:45 +0300

    added button to admit text from text field

--- src/wordInputField.tsx
+++ src/wordInputField.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { TextInput, StyleSheet } from "react-native";
+import { TextInput, StyleSheet, View, Button } from "react-native";
 
 interface WordInputProps {
   onAddWord: (word: string) => void;
@@ -10,31 +10,65 @@ const WordInputField: FC<WordInputProps> = ({ onAddWord }) => {
 
   const inputRef = React.useRef(null);
 
-  const onSubmitEditing = (event: any) => {
-    const newText = event.nativeEvent.text;
-    if (!newText) return;
+  const onSubmitEditing = (newText: string) => {
     onAddWord(newText);
     setInputText("");
     inputRef.current.focus();
   };
 
-  const { inputStyle } = StyleSheet.create({
-    inputStyle: {
-      borderWidth: 1,
-    },
-  });
+  const { inputStyle, inputAddButton, inputField, inputContainer } =
+    StyleSheet.create({
+      inputAddButton: {
+        flex: 1,
+        backgroundColor: "blue",
+      },
+      inputField: {
+        flex: 5,
+        backgroundColor: "red",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      inputStyle: {
+        borderWidth: 1,
+      },
+      inputContainer: {
+        flex: 1,
+        backgroundColor: "pink",
+        marginVertical: 10,
+        flexDirection: "row",
+      },
+    });
 
   return (
-    <TextInput
-      style={inputStyle}
-      ref={inputRef}
-      autoFocus={true}
-      placeholder="enter a new word"
-      blurOnSubmit={true}
-      value={inputText}
-      onChangeText={(text) => setInputText(text)}
-      onSubmitEditing={onSubmitEditing}
-    />
+    <View style={inputContainer}>
+      <View style={inputField}>
+        <TextInput
+          style={inputStyle}
+          ref={inputRef}
+          autoFocus={true}
+          placeholder="enter a new word"
+          blurOnSubmit={true}
+          value={inputText}
+          onChangeText={(text) => {
+            setInputText(text);
+          }}
+          onSubmitEditing={(event) => {
+            const newText = event.nativeEvent.text;
+            if (newText) onSubmitEditing(newText);
+          }}
+        />
+      </View>
+
+      <View style={inputAddButton}>
+        <Button
+          disabled={inputText === ""}
+          title={"add"}
+          onPress={() => {
+            if (inputText != "") onSubmitEditing(inputText);
+          }}
+        ></Button>
+      </View>
+    </View>
   );
 };
 export default WordInputField;
