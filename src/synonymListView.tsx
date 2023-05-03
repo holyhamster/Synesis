3aaa805e656283135a332b84aedb535416f16a40
Dmitrii Mv <hotrhamster@gmail.com>
2023-05-03 13:59:12 +0300

    moved entry building into syndef

--- src/synonymListView.tsx
+++ src/synonymListView.tsx
@@ -1,45 +1,24 @@
 import React, { FC } from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
-import SynDefinition from "./synDefinition";
 import { TagCloud } from "react-tagcloud/rn";
+import DataEntry from "./dataEntry";
 
 interface SynonymListProps {
-  synArray: SynDefinition[];
+  entries: DataEntry[];
   addWord: (newWord: string) => void;
 }
 
-const SynonymListView: FC<SynonymListProps> = ({
-  synArray: synArray,
-  addWord,
-}) => {
+const SynonymListView: FC<SynonymListProps> = ({ entries, addWord }) => {
   const [extended, setExtended] = React.useState<Boolean>(false);
-
-  const data = buildCloudData(synArray);
 
   return (
     <TagCloud
       minSize={20}
       maxSize={35}
-      tags={data}
+      tags={entries}
       shuffle={false}
       onPress={(tag) => addWord(tag.value)}
     />
   );
 };
 
-function buildCloudData(syns: SynDefinition[]): any {
-  const occurances: Map<string, any> = new Map();
-  syns.forEach((synDef) => {
-    synDef.Sets.forEach((synArray) => {
-      synArray.forEach((synonym) => {
-        let data = occurances.get(synonym);
-        if (!data) {
-          data = { value: synonym, color: synDef.Color, count: 30 };
-          occurances.set(synonym, data);
-        } else data.count += 1;
-      });
-    });
-  });
-  return Array.from(occurances.values());
-}
 export default SynonymListView;
