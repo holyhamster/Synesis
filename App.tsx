98d78f5777c86f9f8a89a57acc9daabecf535fe8
Dmitrii Mv <hotrhamster@gmail.com>
2023-05-06 16:54:48 +0300

    added typechecking to navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  NativeStackScreenProps,
+  createNativeStackNavigator,
+} from "@react-navigation/native-stack";
 
 import SynonymScreen from "./src/screens/synonymScreen";
 import OptionsScreen from "./src/screens/optionsScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import InputModal from "./src/screens/inputModal";
 
-const Stack = createNativeStackNavigator();
+export type StackParamList = {
+  Home: undefined;
+  Options: undefined;
+  InputModal: {
+    eventName: string;
+    varName: string;
+    varHint: string | undefined;
+  };
+};
+export type OptionsProps = NativeStackScreenProps<StackParamList, "Options">;
+export type InputModalProps = NativeStackScreenProps<
+  StackParamList,
+  "InputModal"
+>;
+
+const Stack = createNativeStackNavigator<StackParamList>();
 
 export default function App() {
   return (
@@ -19,6 +38,9 @@ export default function App() {
             options={{ headerShown: false }}
           />
           <Stack.Screen name="Options" component={OptionsScreen} />
+          <Stack.Group screenOptions={{ presentation: "modal" }}>
+            <Stack.Screen name="InputModal" component={InputModal} />
+          </Stack.Group>
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
