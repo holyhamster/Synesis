931390fa88cd8d77c0023efde6e503b2199188d1
Dmitrii Mv <hotrhamster@gmail.com>
2023-05-16 10:53:53 +0300

    moved coloredgradient into separate file

--- src/screens/synonyms/synonymWord.tsx
+++ src/screens/synonyms/synonymWord.tsx
@@ -2,21 +2,24 @@ import { LinearGradient } from "expo-linear-gradient";
 import React, { FC } from "react";
 import { Animated, StyleProp, StyleSheet, Text, TextStyle } from "react-native";
 import ColorNormal from "./data/colorNormal";
+import ColorGradient from "./data/colorGradient";
 
 interface SynonymWordProps {
   style?: StyleProp<TextStyle>;
   word: string;
   colorNormal: ColorNormal;
 }
+
 const SynonymWord: FC<SynonymWordProps> = ({ style, word, colorNormal }) => {
   const [gradient, setGradient] = React.useState(
-    colorMapToLinearArray(colorNormal)
+    new ColorGradient(colorNormal)
   );
 
   React.useEffect(() => {
-    setGradient(colorMapToLinearArray(colorNormal));
+    setGradient(new ColorGradient(colorNormal));
   }, [colorNormal]);
 
+  //animate mounting/unmounting of the component
   const animFaded = React.useState(new Animated.Value(0))[0];
   React.useEffect(() => {
     Animated.timing(animFaded, {
@@ -24,7 +27,6 @@ const SynonymWord: FC<SynonymWordProps> = ({ style, word, colorNormal }) => {
       duration: 1000,
       useNativeDriver: true,
     }).start();
-
     return () => {
       Animated.timing(animFaded, {
         toValue: 0,
@@ -50,20 +52,4 @@ const SynonymWord: FC<SynonymWordProps> = ({ style, word, colorNormal }) => {
 };
 
 const styles = StyleSheet.create({ word: { margin: 5 } });
-
-function colorMapToLinearArray(
-  colorNormal: { color: string; value: number }[]
-) {
-  const gradientColors: string[] = [];
-  const gradientLocations: number[] = [];
-  let i: number = 0;
-  colorNormal.forEach((pair) => {
-    gradientColors.push(pair.color, pair.color);
-    gradientLocations.push(i);
-    i += 1 / colorNormal.length;
-    gradientLocations.push(i);
-  });
-  return { colors: gradientColors, locations: gradientLocations };
-}
-
 export default SynonymWord;
