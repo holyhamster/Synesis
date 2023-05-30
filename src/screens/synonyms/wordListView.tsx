28bddb8300fef66972e9458594dfec543db89889
Dmitrii Mv <hotrhamster@gmail.com>
2023-05-30 19:05:52 +0300

    added transition view

--- src/screens/synonyms/wordListView.tsx
+++ src/screens/synonyms/wordListView.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import React, { FC } from "react";
 import {
   Button,
   Pressable,
@@ -11,6 +11,12 @@ import {
 } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import SynonymCollection from "../../dictionaries/data/synonymCollection";
+import { UIGrey } from "../../colors";
+import {
+  Transition,
+  Transitioning,
+  TransitioningView,
+} from "react-native-reanimated";
 
 interface WordListProps {
   synArray: SynonymCollection[];
@@ -28,10 +34,18 @@ const WordListView: FC<WordListProps> = ({
   onClearButton,
   style,
 }) => {
+  React.useEffect(() => {
+    transRef.current.animateNextTransition();
+  }, [synArray]);
+  const transRef = React.useRef<TransitioningView>();
   //todo: add haptic feedback to long press with react-native-haptic-feedback
   return (
     <>
-      <View style={[styles.selectedContainer, style]}>
+      <Transitioning.View
+        style={[styles.selectedContainer, style]}
+        transition={transition}
+        ref={transRef}
+      >
         <View style={styles.selectedList}>
           {synArray.map((synDef, index) => (
             <Pressable
@@ -43,48 +57,55 @@ const WordListView: FC<WordListProps> = ({
               onPress={() => onWordPress(synDef.Word)}
               onLongPress={() => onLongPress(synDef.Word)}
             >
-              <Text style={{ margin: 5 }}>{synDef.Word}</Text>
+              <Text style={styles.text}>{synDef.Word}</Text>
             </Pressable>
           ))}
         </View>
-        <View style={styles.selectedClearButton}>
-          <TouchableOpacity
-            onPress={onClearButton}
-            disabled={synArray.length == 0}
-          >
-            <View style={{ alignItems: "center" }}>
-              <MaterialIcons name="clear" size={32} color="black" />
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
+
+        <TouchableOpacity
+          style={styles.selectedClearButton}
+          onPress={onClearButton}
+          disabled={synArray.length == 0}
+        >
+          <MaterialIcons name="clear" size={40} color="black" />
+        </TouchableOpacity>
+      </Transitioning.View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  text: { fontSize: 20, marginHorizontal: 7, marginVertical: 5 },
   selectedContainer: {
     paddingVertical: 5,
     flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "orange",
+    alignItems: "flex-end",
   },
   selectedList: {
-    flex: 4,
+    flex: 5,
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "flex-start",
     flexWrap: "wrap",
     alignSelf: "stretch",
     alignContent: "center",
     alignItems: "center",
     marginHorizontal: 5,
     gap: 5,
-    backgroundColor: "yellow",
   },
   selectedClearButton: {
-    flex: 1,
-    backgroundColor: "yellow",
+    backgroundColor: UIGrey,
+    alignItems: "center",
+    borderWidth: 2,
+    borderRadius: 15,
+    marginHorizontal: 5,
   },
 });
+
+const transition = (
+  <Transition.Together>
+    <Transition.In type="fade" durationMs={500} />
+    <Transition.Out type="fade" durationMs={500} />
+  </Transition.Together>
+);
 
 export default WordListView;
