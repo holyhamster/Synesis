7863004da0fc260156a0831016fc011a3e91df56
Dmitrii Mv <hotrhamster@gmail.com>
2023-06-09 10:30:15 +0300

    moved building of clouds into list

--- src/screens/synonyms/synonymList.tsx
+++ src/screens/synonyms/synonymList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo, useEffect, useRef, memo, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import { FlatList, ScrollView, StyleSheet } from "react-native";
 import ColorNormal from "./gradient/colorNormal";
 import {
@@ -6,98 +6,91 @@ import {
   Transitioning,
   TransitioningView,
 } from "react-native-reanimated";
-import SynonymCloud from "../../dictionaries/data/synonymCloud";
+import SynonymCloud, {
+  CrossReference,
+} from "../../dictionaries/data/synonymCloud";
 import SynonymWord from "./synonymWord";
+import SynonymCollection from "../../dictionaries/data/synonymCollection";
 
 interface SynonymListProps {
-  clouds: Map<string, SynonymCloud>;
+  synArray: SynonymCollection[];
   colorMap: Map<string, string>;
   highlightedWord?: string;
   addNewWord: (newWord: string) => void;
   tileLayout?: boolean;
 }
 
-const SynonymList: FC<SynonymListProps> = memo(
-  ({ clouds, colorMap, highlightedWord, addNewWord, tileLayout }) => {
-    //console.log(`SynonymList render`);
+const SynonymList: FC<SynonymListProps> = ({
+  synArray,
+  colorMap,
+  highlightedWord,
+  addNewWord,
+  tileLayout,
+}) => {
+  //console.log(`SynonymList render`);
 
-    const [colorNormals, setColorNormals] = useState(
-      new Map<string, ColorNormal>()
+  const [colorNormals, setColorNormals] = useState(
+    new Map<string, ColorNormal>()
+  );
+
+  const [sortedEntries, setSortedEntries] = useState<SynonymCloud[]>([]);
+  const transitionView = useRef<TransitioningView>();
+
+  useEffect(() => {
+    const newClouds = CrossReference(synArray);
+    setSortedEntries(SynonymCloud.GetSorted(newClouds, highlightedWord));
+    if (tileLayout) transitionView.current?.animateNextTransition();
+  }, [synArray]);
+
+  useEffect(() => {
+    setSortedEntries(SynonymCloud.GetSorted(sortedEntries, highlightedWord));
+  }, [highlightedWord]);
+
+  useEffect(() => {
+    setColorNormals(rebuildNormals(sortedEntries, colorMap, colorNormals));
+  }, [sortedEntries, colorMap]);
+
+  const renderCloud = (cloud: SynonymCloud) => {
+    return (
+      <SynonymWord
+        key={cloud.name}
+        word={cloud.name}
+        colorNormal={colorNormals.get(cloud.name)}
+        onPress={addNewWord}
+      />
     );
-
-    const [sortedEntries, setSortedEntries] = useState<SynonymCloud[]>([]);
-    const transitionView = useRef<TransitioningView>();
-
-    useEffect(() => {
-      const newSorted = Array.from(clouds.values());
-      if (highlightedWord?.length > 0)
-        newSorted.sort(
-          (a, b) =>
-            (a.GetWordMap().get(highlightedWord) || 0) -
-            (b.GetWordMap().get(highlightedWord) || 0)
-        );
-      setSortedEntries(newSorted);
-      if (tileLayout) transitionView.current?.animateNextTransition();
-    }, [clouds, highlightedWord]);
-
-    useEffect(() => {
-      const map = new Map();
-      clouds.forEach((cloud, name) => {
-        const oldNormal = colorNormals.get(name);
-        const newNormal = new ColorNormal(cloud.GetWordNormal(), colorMap);
-        map.set(name, oldNormal?.isEqual(newNormal) ? oldNormal : newNormal);
-      });
-      setColorNormals(map);
-    }, [colorMap, clouds]);
-
-    const renderCloud = (cloud) => {
-      const colorNormal = colorNormals.get(cloud.name);
-      if (colorNormal?.length > 0)
-        return (
-          <SynonymWord
-            key={cloud.name}
-            word={cloud.name}
-            colorNormal={colorNormal}
-            onPress={() => addNewWord(cloud.name)}
-          />
-        );
-      return <></>;
-    };
-
-    if (tileLayout) {
-      return (
-        <ScrollView
-          keyboardShouldPersistTaps="handled"
-          style={styles.synonymScroll}
-          fadingEdgeLength={1}
-          snapToEnd={true}
-          contentContainerStyle={styles.synonymScrollContainer}
+  };
+  if (tileLayout) {
+    return (
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        style={styles.synonymScroll}
+        fadingEdgeLength={1}
+        snapToEnd={true}
+        contentContainerStyle={styles.synonymScrollContainer}
+      >
+        <Transitioning.View
+          ref={transitionView}
+          style={styles.list}
+          transition={transition}
         >
-          <Transitioning.View
-            ref={transitionView}
-            style={styles.list}
-            transition={transition}
-          >
-            {sortedEntries
-              .slice(-wordLimit, -1)
-              .map((cloud) => renderCloud(cloud))}
-          </Transitioning.View>
-        </ScrollView>
-      );
-    } else
-      return (
-        <FlatList
-          data={sortedEntries}
-          keyExtractor={(entry) => entry.name}
-          renderItem={({ item }) => renderCloud(item)}
-          keyboardShouldPersistTaps="handled"
-          fadingEdgeLength={1}
-          snapToEnd={true}
-          contentContainerStyle={styles.flatListContainer}
-        />
-      );
-  }
-);
+          {sortedEntries.slice(-wordLimit).map((cloud) => renderCloud(cloud))}
+        </Transitioning.View>
+      </ScrollView>
+    );
+  } else
+    return (
+      <FlatList
+        data={sortedEntries}
+        keyExtractor={(entry) => entry.name}
+        renderItem={({ item }) => renderCloud(item)}
+        keyboardShouldPersistTaps="handled"
+        fadingEdgeLength={1}
+        snapToEnd={true}
+        contentContainerStyle={styles.flatListContainer}
+      />
+    );
+};
 const wordLimit = 40;
 
 const styles = StyleSheet.create({
@@ -129,6 +122,20 @@ const styles = StyleSheet.create({
   },
 });
 
+function rebuildNormals(
+  clouds: SynonymCloud[],
+  colorMap: Map<string, string>,
+  oldNormals: Map<string, ColorNormal>
+) {
+  const map = new Map<string, ColorNormal>();
+  clouds.forEach((cloud) => {
+    const oldNormal = oldNormals.get(cloud.name);
+    const newNormal = new ColorNormal(cloud.GetWordNormal(), colorMap);
+    if (!newNormal.IsValid) return;
+    map.set(cloud.name, oldNormal?.isEqual(newNormal) ? oldNormal : newNormal);
+  });
+  return map;
+}
 const transition = (
   <Transition.Together>
     <Transition.In type="fade" durationMs={500} />
