cbd7b5d06c14a75738dbe7a7b6b32fea84a98b07
Dmitrii Mv <hotrhamster@gmail.com>
2023-06-11 13:10:56 +0300

    comments

--- src/screens/synonyms/gradient/colorGradient.ts
+++ src/screens/synonyms/gradient/colorGradient.ts
@@ -1,5 +1,7 @@
 import ColorNormal from "./colorNormal";
 
+//translates ColorNormal into a gradient for GradientRect component
+//["red": 0.5, "blue": 0.5] => {colors: ["red", "red", "blue", "blur"], locations:[0, .5, .5, 1]}
 export default class ColorGradient {
   colors: string[];
   locations: number[];
