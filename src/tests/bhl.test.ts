7d893e245077a145743bbe04ddde5f52ab87bb0e
Dmitrii Mv <hotrhamster@gmail.com>
2023-06-26 11:23:27 +0300

    added BHL API

---
+++ src/tests/bhl.test.ts
@@ -0,0 +1,23 @@
+import BuildBHL, { BHLParse } from "../dictionaries/bighugelabs";
+import { APIErrorEnum } from "../dictionaries/data/apiResponse";
+import SynonymCollection from "../dictionaries/data/synonymCollection";
+
+const API_KEY = `87871e951adf8f98d4c434e9ca979587`;
+const word = "hand";
+const wordMockResponse = `{"noun":{"syn":["manus","mitt","paw","hired hand","hired man","handwriting","script","deal","bridge player","helping hand","ability","accumulation","aggregation","aid","applause","assemblage","assist","assistance","card player","clapping","collection","crewman","extremity","forepaw","hand clapping","handbreadth","handsbreadth","help","jack","laborer","labourer","manual laborer","pointer","power","sailor","side","writing"]},"verb":{"syn":["pass","reach","pass on","turn over","give","conduct","direct","guide","lead","take","transfer"],"rel":["pass on","hand down","hand out","hand over"]}}`;
+
+const typoWord = "sdfamsdfl";
+
+test("BHL parse test", async () => {
+  const parser = BHLParse;
+  let parsed = await parser(word, new Response(wordMockResponse));
+  expect(parsed.type == "success" ? parsed.data.length : 0).toBeGreaterThan(0);
+});
+
+test("live BHL test", async () => {
+  const dictionary = BuildBHL(`87871e951adf8f98d4c434e9ca979587`);
+  const synonym = new SynonymCollection(`hand`);
+  const response = await synonym.Load(dictionary);
+
+  expect(synonym.definitionSets).toBe(0);
+});
