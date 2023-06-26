0540535da613af06fa9b8aede35639c7f6f94eec
Dmitrii Mv <hotrhamster@gmail.com>
2023-06-26 11:23:03 +0300

    replaced securestore with storage on web

--- src/dictionaries/storageHandling.ts
+++ src/dictionaries/storageHandling.ts
@@ -1,16 +1,18 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export async function GetStringFromStorage(storageString: string) {
-  return AsyncStorage.getItem(storageString);
-}
+export default class Storage {
+  static async GetString(storageString: string) {
+    return AsyncStorage.getItem(storageString);
+  }
 
-export async function SetStringInStorage(storageString: string, value: string) {
-  return AsyncStorage.setItem(storageString, value);
+  static async SetString(storageString: string, value: string) {
+    return AsyncStorage.setItem(storageString, value);
+  }
 }
 
 export enum StringTypesEnum {
   WasLaunched = "WasLaunched",
-  DisabledVisulats = "DisabledVisulats",
+  DisabledVisualts = "DisabledVisuals",
   TileLayout = "TileLayout",
   TileCount = "TileCount",
 }
