1772082752a95e3979501e755faab75c504ca262
Dmitrii Mv <hotrhamster@gmail.com>
2023-06-26 14:58:25 +0300

    updated tests

--- src/tests/datamuse.test.ts
+++ src/tests/datamuse.test.ts
@@ -1,28 +1,36 @@
 import { APIErrorEnum } from "../dictionaries/data/apiResponse";
 import BuildDatamuse, { DatamuseParse } from "../dictionaries/datamuse";
-import { CrossReference } from "../dictionaries/data/synonymCloud";
-import SynonymCollection from "../dictionaries/data/synonymCollection";
 
+const word = "hand";
 const mockresponse = `[{"word":"list","score":5054},{"word":"lean","score":3439},{"word":"propensity","score":2071},{"word":"tilt","score":1472},{"word":"proclivity","score":1374},{"word":"inclination","score":1268},{"word":"tendency","score":1137},{"word":"inclined","score":903},{"word":"tilted","score":363},{"word":"canted","score":298},{"word":"atilt","score":277},{"word":"tipped","score":231},{"word":"unerect","score":29}]`;
 
-test("Datamuse parse test", async () => {
-  const parser = DatamuseParse;
-  const response: Response = new Response(mockresponse, { status: 200 });
-  var parsed = await parser("list", response);
-  expect(parsed.type == "success" ? parsed.data.length : 0).toBeGreaterThan(0);
+test("parsing", async () => {
+  const response: Response = new Response(mockresponse);
+  var parsed = await DatamuseParse(word, response);
 
-  parsed = await parser("", new Response("[]"));
-  expect(parsed.type == "error" ? parsed.errorMessage : 0).toBe(
-    APIErrorEnum.NoWord
-  );
+  expect(parsed.type).toBe("success");
+  expect((parsed as any).data.length).toBeGreaterThan(0);
 });
 
-const datamuseDictionary = BuildDatamuse();
-test("datamuse crossing", async () => {
-  const synDef1 = new SynonymCollection("leaning");
-  const synDef2 = new SynonymCollection("tilted");
+const validDictionary = BuildDatamuse();
 
-  await synDef1.Load(datamuseDictionary);
-  await synDef2.Load(datamuseDictionary);
-  expect(CrossReference([synDef1, synDef2])).toBeTruthy();
+test("successful fetch and parse", async () => {
+  const response = await validDictionary.GetSynonyms(word);
+
+  expect(response.type).toBe("success");
+  expect((response as any).data.length).toBeGreaterThan(0);
+});
+
+test("non existant word", async () => {
+  const response = await validDictionary.GetSynonyms("asfdasdasd");
+
+  expect(response.type).toBe("error");
+  expect((response as any).errorMessage).toBe(APIErrorEnum.NoWord);
+});
+
+test.skip("network unavailiable", async () => {
+  const response = await validDictionary.GetSynonyms(word);
+
+  expect(response.type).toBe("error");
+  expect((response as any).errorMessage).toBe(APIErrorEnum.Network);
 });
--- src/tests/meriam.test.ts
+++ src/tests/meriam.test.ts
@@ -1,38 +1,44 @@
 import { APIErrorEnum } from "../dictionaries/data/apiResponse";
 import BuildMeriam, { MeriamParse } from "../dictionaries/meriam";
 
+const word = "points of view";
 const meriamMockResponse = `[{\"meta\":{\"id\":\"points of view\",\"uuid\":\"06da68ed-607d-44b6-88b6-cee51b552ec5\",\"src\":\"CTenhance\",\"section\":\"phrases\",\"stems\":[\"points of view\"],\"syns\":[[\"angles\",\"eye views\",\"outlooks\",\"perspectives\",\"shoes\",\"slants\",\"standpoints\",\"vantage points\",\"viewpoints\"]],\"ants\":[],\"offensive\":false},\"hwi\":{\"hw\":\"points of view\"},\"fl\":\"phrase\",\"sls\":[\"plural of {d_link|point of view|point of view}\"],\"def\":[{\"sseq\":[[[\"sense\",{\"dt\":[[\"text\",\"a way of looking at or thinking about something \"],[\"vis\",[{\"t\":\"From my {it}point of view{\\/it}, the best decision is to quit while we\\u0027re ahead.\"}]]],\"syn_list\":[[{\"wd\":\"angles\"},{\"wd\":\"eye views\"},{\"wd\":\"outlooks\"},{\"wd\":\"perspectives\"},{\"wd\":\"shoes\"},{\"wd\":\"slants\"},{\"wd\":\"standpoints\"},{\"wd\":\"vantage points\"},{\"wd\":\"viewpoints\"}]],\"rel_list\":[[{\"wd\":\"interpretations\"},{\"wd\":\"spins\"}],[{\"wd\":\"beliefs\"},{\"wd\":\"convictions\"},{\"wd\":\"eyes\"},{\"wd\":\"feelings\"},{\"wd\":\"judgments\",\"wvrs\":[{\"wvl\":\"or\",\"wva\":\"judgements\"}]},{\"wd\":\"minds\"},{\"wd\":\"mind-sets\"},{\"wd\":\"notions\"},{\"wd\":\"opinions\"},{\"wd\":\"perceptions\"},{\"wd\":\"persuasions\"},{\"wd\":\"sentiments\"},{\"wd\":\"verdicts\"},{\"wd\":\"views\"}],[{\"wd\":\"impressions\"},{\"wd\":\"takes\"}]],\"phrase_list\":[[{\"wd\":\"frames of reference\"}]]}]]]}],\"shortdef\":[\"a way of looking at or thinking about something\"]},{\"meta\":{\"id\":\"point of view\",\"uuid\":\"8446791b-ce99-4c36-9afa-91520d426674\",\"src\":\"CTenhance\",\"section\":\"phrases\",\"stems\":[\"point of view\",\"points of view\"],\"syns\":[[\"angle\",\"eye view\",\"outlook\",\"perspective\",\"shoes\",\"slant\",\"standpoint\",\"vantage point\",\"viewpoint\"]],\"ants\":[],\"offensive\":false},\"hwi\":{\"hw\":\"point of view\"},\"fl\":\"phrase\",\"def\":[{\"sseq\":[[[\"sense\",{\"dt\":[[\"text\",\"a way of looking at or thinking about something \"],[\"vis\",[{\"t\":\"From my {it}point of view{\\/it}, the best decision is to quit while we\\u0027re ahead.\"}]]],\"syn_list\":[[{\"wd\":\"angle\"},{\"wd\":\"eye view\"},{\"wd\":\"outlook\"},{\"wd\":\"perspective\"},{\"wd\":\"shoes\"},{\"wd\":\"slant\"},{\"wd\":\"standpoint\"},{\"wd\":\"vantage point\"},{\"wd\":\"viewpoint\"}]],\"rel_list\":[[{\"wd\":\"interpretation\"},{\"wd\":\"spin\"}],[{\"wd\":\"belief\"},{\"wd\":\"conviction\"},{\"wd\":\"eye\"},{\"wd\":\"feeling\"},{\"wd\":\"judgment\",\"wvrs\":[{\"wvl\":\"or\",\"wva\":\"judgement\"}]},{\"wd\":\"mind\"},{\"wd\":\"mind-set\"},{\"wd\":\"notion\"},{\"wd\":\"opinion\"},{\"wd\":\"perception\"},{\"wd\":\"persuasion\"},{\"wd\":\"sentiment\"},{\"wd\":\"verdict\"},{\"wd\":\"view\"}],[{\"wd\":\"impression\"},{\"wd\":\"take\"}]],\"phrase_list\":[[{\"wd\":\"frame of reference\"}]]}]]]}],\"shortdef\":[\"a way of looking at or thinking about something\"]}]`;
 
-const meriamParser = MeriamParse;
-test("API parsing", async () => {
-  const response = await meriamParser(
-    "points of view",
-    new Response(meriamMockResponse)
-  );
-  expect(
-    response.type == "success" ? response.data.flat(3).length : 0
-  ).toBeGreaterThan(0);
+test("parsing", async () => {
+  const response = await MeriamParse(word, new Response(meriamMockResponse));
+  expect(response.type).toBe("success");
+  expect((response as any).data.length).toBeGreaterThan(0);
 });
 
-test("API key rejection", async () => {
-  var wrongAPIdict = BuildMeriam("1234-1234");
-  const wrongAPIresponse = await wrongAPIdict.GetSynonyms("points_of_vew");
-  expect(
-    wrongAPIresponse.type == "error" ? wrongAPIresponse.errorMessage : ""
-  ).toBe(APIErrorEnum.WrongAPIkey);
+const API_KEY = ``;
+const validDictionary = API_KEY ? BuildMeriam(API_KEY) : undefined;
+const wrongKeyDictionary = BuildMeriam("1234");
+
+test("successful fetch and parse", async () => {
+  expect(validDictionary).toBeTruthy();
+
+  const response = await validDictionary.GetSynonyms(word);
+
+  expect(response.type).toBe("success");
+  expect((response as any).data.length).toBeGreaterThan(0);
 });
 
-const meriamMockMisspeled = `["band","waned","want","hand","wound","land","bond","sand","wont","wad","wend","wan","wind","wane","windy","ward","went","bland","brand","grand"]`;
-test("API parsing, misspelled responses", async () => {
-  const response = await meriamParser(
-    "wand",
-    new Response(meriamMockMisspeled)
-  );
-  expect(response.type == "error" ? response.errorMessage : undefined).toBe(
-    APIErrorEnum.NoWord
-  );
-  const emptyResponse = await meriamParser("fasdnssobkjfd", new Response(`[]`));
-  expect(
-    emptyResponse.type == "error" ? emptyResponse.errorMessage : undefined
-  ).toBe(APIErrorEnum.NoWord);
+test("wrong key", async () => {
+  const response = await wrongKeyDictionary.GetSynonyms("points_of_vew");
+  expect(response.type).toBe("error");
+  expect((response as any).errorMessage).toBe(APIErrorEnum.WrongAPIkey);
+});
+
+test("non existant word", async () => {
+  expect(validDictionary).toBeTruthy();
+  const response = await validDictionary.GetSynonyms("asfskjdfnskjdf");
+  expect(response.type).toBe("error");
+  expect((response as any).errorMessage).toBe(APIErrorEnum.NoWord);
+});
+
+test.skip("network unavailiable", async () => {
+  const response = await wrongKeyDictionary.GetSynonyms(word);
+
+  expect(response.type).toBe("error");
+  expect((response as any).errorMessage).toBe(APIErrorEnum.Network);
 });
